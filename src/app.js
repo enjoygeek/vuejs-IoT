c598b82918d751d245cc1ca930b6911590bd3ca5
Justin Neff <justinkneff@gmail.com>
2016-10-03 15:44:58 -0500

    Following the initial directions to create an app.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,46 @@
 import $ from 'jquery';
-
-new Vue({
+//because we're reactive we can go to the console and type app.message
+window.app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!!'
     }
+});
+//go to console and check the id, app2.id
+window.app2 = new Vue({
+    el: '#app-2',
+    data: {
+        id: 'inspect-me'
+    }
+});
+
+//enter app3.seen = false in the console.
+window.app3 = new Vue ({
+    el: "#app-3",
+    data: {
+        seen: true
+    }
+});
+//app4.todos.push({ text: 'New item' })
+window.app4 = new Vue({
+    el: "#app-4",
+    data: {
+        todos: [
+            { text: 'Learn Javascript'},
+            { text: 'Learn Vue'},
+            { text: 'Build something awesome'}
+        ]
+    }
+});
+
+window.app5 = new Vue({
+     el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
 });
